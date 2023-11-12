625ee762ec7e8720da58f7a27093d276723c5303
Clément Renault <clement@meilisearch.com>
2023-11-12 16:36:07 +0100

    First non-compiling build impl

--- src/heed_reader.rs
+++ src/heed_reader.rs
@@ -4,14 +4,18 @@ use std::collections::BinaryHeap;
 use std::mem::size_of;
 use std::{iter, mem};
 
-use bytemuck::{pod_collect_to_vec, Pod, Zeroable};
+use annoy_rs::utils::dot_product_no_simd;
+use bytemuck::{bytes_of, cast_slice, pod_collect_to_vec, Pod, Zeroable};
 use byteorder::{ByteOrder, NativeEndian};
 use heed::types::ByteSlice;
 use heed::{BoxedError, BytesDecode, BytesEncode, Database, RoTxn, RwTxn};
 use ordered_float::OrderedFloat;
+use rand::seq::index::{IndexVec, IndexVecIter};
+use rand::seq::SliceRandom;
+use rand::Rng;
 
 use crate::distance::{
-    cosine_distance_no_simd, dot_product_no_simd, euclidean_distance_no_simd,
+    self, cosine_distance_no_simd, dot_product_no_simd, euclidean_distance_no_simd,
     manhattan_distance_no_simd, minkowski_margin,
 };
 use crate::priority_queue::BinaryHeapItem;
@@ -125,6 +129,185 @@ impl HeedReader {
         }
 
         Ok(())
+    }
+
+    // TODO we must unbuild to do that safely
+    pub fn add_item(&self, wtxn: &mut RwTxn, item: u32, vector: &[f32]) -> heed::Result<()> {
+        assert_eq!(
+            vector.len(),
+            self.dimension,
+            "invalid vector dimensions, provided {} but expected {}",
+            vector.len(),
+            self.dimension
+        );
+
+        // TODO use with_capacity
+        let mut vec = Vec::new();
+        let leaf = Leaf::from_vector(self.distance_type, vector);
+        self.database.put(wtxn, &item, leaf.append_to_vec(&mut vec))
+    }
+
+    pub fn build<R: Rng>(&self, wtxn: &mut RwTxn, mut rng: R, n_trees: usize) -> heed::Result<()> {
+        assert!(n_trees >= 1, "please provide a n_trees >= 1 for now");
+
+        todo!("preprocess stuff");
+        todo!("n_nodes = n_items (no tree nodes yet)");
+
+        let mut thread_roots = Vec::new();
+        while thread_roots.len() < n_trees {
+            // TODO use a roaring bitmap?
+            let mut indices = Vec::new();
+            for result in self.database.iter(wtxn)? {
+                let (i, _) = result?;
+                indices.push(i);
+            }
+
+            let tree_root_id = self.make_tree(wtxn, &indices, true)?;
+            thread_roots.push(tree_root_id);
+        }
+
+        todo!("copy roots in the database");
+        todo!("postprocess stuff");
+
+        Ok(())
+    }
+
+    fn make_tree(&self, wtxn: &mut RwTxn, indices: &[u32], is_root: bool) -> heed::Result<u32> {
+        // The basic rule is that if we have <= _K items, then it's a leaf node, otherwise it's a split node.
+        // There's some regrettable complications caused by the problem that root nodes have to be "special":
+        // 1. We identify root nodes by the arguable logic that _n_items == n->n_descendants, regardless of how many descendants they actually have
+        // 2. Root nodes with only 1 child need to be a "dummy" parent
+        // 3. Due to the _n_items "hack", we need to be careful with the cases where _n_items <= _K or _n_items > _K
+        if indices.len() == 1 && !is_root {
+            return Ok(indices[0]);
+        }
+
+        // if (indices.size() <= (size_t)_K && (!is_root || (size_t)_n_items <= (size_t)_K || indices.size() == 1)) {
+        let n_items: usize = 0; // We must count the number of items
+        if indices.len() <= self.max_descendants
+            && (!is_root || n_items <= self.max_descendants || indices.len() == 1)
+        {
+            let item_id: u32 = self.database.len(wtxn)?.try_into().unwrap();
+
+            // threaded_build_policy.lock_n_nodes();
+            // _allocate_size(_n_nodes + 1, threaded_build_policy);
+            // S item = _n_nodes++;
+            // threaded_build_policy.unlock_n_nodes();
+
+            // threaded_build_policy.lock_shared_nodes();
+            // Node* m = _get(item);
+            // m->n_descendants = is_root ? _n_items : (S)indices.size();
+
+            // We identify root nodes by the arguable logic that _n_items == n->n_descendants,
+            // regardless of how many descendants they actually have
+            let n_descendants = if is_root { n_items } else { indices.len() } as u32;
+            let node = Descendants { n_descendants, descendants_bytes: cast_slice(indices) };
+
+            let mut vec = Vec::new();
+            self.database.put(wtxn, &item_id, node.append_to_vec(&mut vec))?;
+            return Ok(item_id);
+        }
+
+        let mut children = Vec::new();
+        for item_id in indices {
+            let node_bytes = self.database.get(wtxn, item_id)?.unwrap();
+            match Node::from_bytes(node_bytes, self.distance_type, self.max_descendants) {
+                Node::Leaf(leaf) => children.push(leaf),
+                Node::Descendants(_) | Node::SplitPlaneNormal(_) => {
+                    panic!("a children must be a leaf ???")
+                }
+            }
+        }
+
+        let mut children_indices[2] = [Vec::new(); 2];
+        let mut m = None;
+
+        for _ in 0..3 {
+            children_indices[0].clear();
+            children_indices[1].clear();
+            D::create_split(children, _f, _s, _random, m);
+
+            for item_id in indices {
+                let node_bytes = self.database.get(wtxn, item_id)?.unwrap();
+                let n = match Node::from_bytes(node_bytes, self.distance_type, self.max_descendants) {
+                    Node::Leaf(leaf) => leaf,
+                    Node::Descendants(_) | Node::SplitPlaneNormal(_) => {
+                        panic!("a children must be a leaf ???")
+                    }
+                };
+
+                bool side = D::side(m, n, _f, _random);
+                children_indices[side].push(item_id);
+            }
+
+            if _split_imbalance(children_indices[0], children_indices[1]) < 0.95 {
+                break;
+            }
+        }
+
+        //     vector<S> children_indices[2];
+        //     Node* m = (Node*)alloca(_s);
+
+        //     for (int attempt = 0; attempt < 3; attempt++) {
+        //       children_indices[0].clear();
+        //       children_indices[1].clear();
+        //       D::create_split(children, _f, _s, _random, m);
+
+        //       for (size_t i = 0; i < indices.size(); i++) {
+        //         S j = indices[i];
+        //         Node* n = _get(j);
+        //         if (n) {
+        //           bool side = D::side(m, n, _f, _random);
+        //           children_indices[side].push_back(j);
+        //         } else {
+        //           annoylib_showUpdate("No node for index %d?\n", j);
+        //         }
+        //       }
+
+        //       if (_split_imbalance(children_indices[0], children_indices[1]) < 0.95)
+        //         break;
+        //     }
+        //     threaded_build_policy.unlock_shared_nodes();
+
+        //     // If we didn't find a hyperplane, just randomize sides as a last option
+        //     while (_split_imbalance(children_indices[0], children_indices[1]) > 0.99) {
+        //       if (_verbose)
+        //         annoylib_showUpdate("\tNo hyperplane found (left has %zu children, right has %zu children)\n",
+        //           children_indices[0].size(), children_indices[1].size());
+
+        //       children_indices[0].clear();
+        //       children_indices[1].clear();
+
+        //       // Set the vector to 0.0
+        //       for (int z = 0; z < _f; z++)
+        //         m->v[z] = 0;
+
+        //       for (size_t i = 0; i < indices.size(); i++) {
+        //         S j = indices[i];
+        //         // Just randomize...
+        //         children_indices[_random.flip()].push_back(j);
+        //       }
+        //     }
+
+        //     int flip = (children_indices[0].size() > children_indices[1].size());
+
+        //     m->n_descendants = is_root ? _n_items : (S)indices.size();
+        //     for (int side = 0; side < 2; side++) {
+        //       // run _make_tree for the smallest child first (for cache locality)
+        //       m->children[side^flip] = _make_tree(children_indices[side^flip], false, _random, threaded_build_policy);
+        //     }
+
+        //     threaded_build_policy.lock_n_nodes();
+        //     _allocate_size(_n_nodes + 1, threaded_build_policy);
+        //     S item = _n_nodes++;
+        //     threaded_build_policy.unlock_n_nodes();
+
+        //     threaded_build_policy.lock_shared_nodes();
+        //     memcpy(_get(item), m, _s);
+        //     threaded_build_policy.unlock_shared_nodes();
+
+        // return item;
+        return Ok(0);
     }
 
     pub fn item_vector(&self, rtxn: &RoTxn, item: u32) -> heed::Result<Option<Vec<f32>>> {
@@ -253,23 +436,73 @@ impl DistanceType {
             DistanceType::Manhattan => distance,
         }
     }
-}
 
-pub enum NodeCodec {}
+    fn create_split<R: Rng>(&self, rng: R, children: &[Leaf]) -> (NodeHeader, Vec<f32>) {
+        fn two_means_dot_product<R: Rng>(mut rng: R, nodes: &[Leaf], cosine: bool) -> [OwnedLeaf; 2] {
+              /*
+                This algorithm is a huge heuristic. Empirically it works really well, but I
+                can't motivate it well. The basic idea is to keep two centroids and assign
+                points to either one of them. We weight each centroid by the number of points
+                assigned to it, so to balance it.
+              */
+              const ITERATION_STEPS: usize = 200;
 
-impl<'a> BytesEncode<'a> for NodeCodec {
-    type EItem = (NodeHeader, &'a [u8]);
+              let mut random_nodes = nodes.choose_multiple(&mut rng, 2);
+              let node_i = random_nodes.next().unwrap();
+              let node_j = random_nodes.next().unwrap();
 
-    fn bytes_encode(item: &'a Self::EItem) -> Result<Cow<'a, [u8]>, BoxedError> {
-        todo!()
-    }
-}
+              let mut node_p = OwnedLeaf::from_leaf(node_i);
+              let mut node_q = OwnedLeaf::from_leaf(node_j);
 
-impl<'a> BytesDecode<'a> for NodeCodec {
-    type DItem = (NodeHeader, &'a [u8]);
+              if cosine {
+                  node_p.normalize();
+                  node_q.normalize();
+              }
 
-    fn bytes_decode(bytes: &'a [u8]) -> Result<Self::DItem, BoxedError> {
-        Ok(NodeHeader::from_bytes(bytes, DistanceType::Angular))
+            // TODO init only init node.norm, we don't care ???
+            //   Distance::init_node(node_p, f);
+            //   Distance::init_node(node_q, f);
+
+              let mut ic = 1;
+              let mut jc = 1;
+              for _ in 0..ITERATION_STEPS {
+                let node_k = nodes.choose(&mut rng).unwrap();
+                T di = ic * Distance::distance(node_p, node_k, f),
+                  dj = jc * Distance::distance(node_q, node_k, f);
+                T norm = cosine ? Distance::template get_norm<T, Node>(node_k, f) : 1;
+                if norm <= 0.0 {
+                    continue;
+                }
+                if di < dj {
+                  Distance::update_mean(node_p, nodes[k], norm, ic, f);
+                  Distance::init_node(node_p, f);
+                  ic += 1;
+                } else if dj < di {
+                  Distance::update_mean(node_q, nodes[k], norm, jc, f);
+                  Distance::init_node(node_q, f);
+                  jc += 1;
+                }
+              }
+
+            [node_p, node_q]
+        }
+
+        // match self {
+        //     DistanceType::Angular => {
+        //         Node<S, T>* p = (Node<S, T>*)alloca(s);
+        //         Node<S, T>* q = (Node<S, T>*)alloca(s);
+        //         DotProduct::zero_value(p);
+        //         DotProduct::zero_value(q);
+        //         two_means<T, Random, DotProduct, Node<S, T> >(nodes, f, random, true, p, q);
+        //         for (int z = 0; z < f; z++)
+        //             n->v[z] = p->v[z] - q->v[z];
+        //         n->dot_factor = p->dot_factor - q->dot_factor;
+        //         DotProduct::normalize<T, Node<S, T> >(n, f);
+        //     }
+        //     DistanceType::Euclidean => todo!(),
+        //     DistanceType::Manhattan => todo!(),
+        //     DistanceType::Dot => todo!(),
+        // }
     }
 }
 
@@ -304,7 +537,9 @@ impl<'a> Node<'a> {
             let (header, vector_bytes) = NodeHeader::from_bytes(bytes, distance_type);
             Node::Leaf(Leaf { header, vector_bytes })
         } else if n_descendants as usize <= max_descendants {
+            // TODO use the appropriate distance
             let offset = NodeHeaderAngular::offset_before_children();
+            // TODO move this logic into the Descendants type
             let length = n_descendants as usize * size_of::<u32>();
             let descendants_bytes = &bytes[offset..offset + length];
             Node::Descendants(Descendants { n_descendants, descendants_bytes })
@@ -323,12 +558,65 @@ impl<'a> Node<'a> {
     }
 }
 
+struct OwnedLeaf {
+    pub header: NodeHeader,
+    pub vector: Vec<f32>,
+}
+
+impl OwnedLeaf {
+    fn from_leaf(leaf: &Leaf) -> OwnedLeaf {
+        OwnedLeaf { header: leaf.header, vector: leaf.vector() }
+    }
+
+    fn as_leaf(&self) -> Leaf {
+        Leaf { header: self.header, vector_bytes: cast_slice(self.vector.as_ref()) }
+    }
+
+    fn normalize(&mut self) {
+        match &mut self.header {
+            NodeHeader::Angular(_) | NodeHeader::Minkowski(_) => {
+                let norm = dot_product_no_simd(&self.vector, &self.vector).sqrt();
+                if norm > 0.0 {
+                    self.vector.iter_mut().for_each(|x| *x /= norm);
+                }
+            },
+            NodeHeader::Dot(NodeHeaderDot { dot_factor, .. }) => {
+                let norm = (dot_product_no_simd(&self.vector, &self.vector) + dot_factor.powi(2)).sqrt();
+                if norm > 0.0 {
+                    self.vector.iter_mut().for_each(|x| *x /= norm);
+                    *dot_factor /= norm;
+                }
+            },
+        }
+    }
+}
+
 struct Leaf<'a> {
     pub header: NodeHeader,
     vector_bytes: &'a [u8],
 }
 
 impl Leaf<'_> {
+    fn from_vector(distance_type: DistanceType, vector: &[f32]) -> Leaf<'_> {
+        let header = match distance_type {
+            DistanceType::Angular => {
+                NodeHeader::Angular(NodeHeaderAngular { n_descendants: 1, children: [0, 0] })
+            }
+            DistanceType::Euclidean => todo!(),
+            DistanceType::Manhattan => todo!(),
+            DistanceType::Dot => todo!(),
+        };
+        Leaf { header, vector_bytes: cast_slice(vector) }
+    }
+
+    fn append_to_vec<'v>(&self, vec: &'v mut Vec<u8>) -> &'v mut [u8] {
+        let Leaf { header, vector_bytes } = self;
+        let len = vec.len();
+        vec.extend_from_slice(header.as_bytes());
+        vec.extend_from_slice(vector_bytes);
+        &mut vec[len..]
+    }
+
     fn vector(&self) -> Vec<f32> {
         let Leaf { header: _, vector_bytes } = self;
         pod_collect_to_vec(vector_bytes)
@@ -341,6 +629,22 @@ struct Descendants<'a> {
 }
 
 impl<'a> Descendants<'a> {
+    fn from_descendants(descendants: &[u32]) -> Descendants {
+        Descendants {
+            n_descendants: descendants.len().try_into().unwrap(),
+            descendants_bytes: cast_slice(descendants),
+        }
+    }
+
+    fn append_to_vec<'v>(&self, vec: &'v mut Vec<u8>) -> &'v mut [u8] {
+        let Descendants { n_descendants, descendants_bytes } = self;
+        let len = vec.len();
+        vec.extend_from_slice(&n_descendants.to_ne_bytes());
+        vec.extend_from_slice(descendants_bytes);
+        // TODO should we pad with zeros to create a page?
+        &mut vec[len..]
+    }
+
     fn descendants_ids(&self) -> impl Iterator<Item = u32> + 'a {
         let Descendants { n_descendants: _, descendants_bytes: mut remaining } = self;
         iter::from_fn(move || {
@@ -381,6 +685,7 @@ impl SplitPlaneNormal<'_> {
 }
 
 #[repr(C)]
+#[derive(Debug, Clone, Copy)]
 pub enum NodeHeader {
     Angular(NodeHeaderAngular),
     Minkowski(NodeHeaderMinkowski),
@@ -402,6 +707,14 @@ impl NodeHeader {
                 let (header, remaining) = NodeHeaderDot::read(bytes);
                 (NodeHeader::Dot(header), remaining)
             }
+        }
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        match self {
+            NodeHeader::Angular(angular) => bytes_of(angular),
+            NodeHeader::Minkowski(minkowski) => bytes_of(minkowski),
+            NodeHeader::Dot(dot) => bytes_of(dot),
         }
     }
 
