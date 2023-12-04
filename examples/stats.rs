e4e4e70b40bf479cc01594c140046d0ceab9883c
Tamo <tamo@meilisearch.com>
2023-12-04 15:38:56 +0100

    compute the averages

--- examples/stats.rs
+++ examples/stats.rs
@@ -24,11 +24,22 @@ fn main() -> anyhow::Result<()> {
     let database: Database<DotProduct> = env.open_database(&rtxn, None)?.unwrap();
     let reader = Reader::open(&rtxn, 0, database)?;
 
+    let mut dummy_sum = 0;
+    let mut depth_sum = 0;
     let Stats { tree_stats } = reader.stats(&rtxn)?;
-    println!("There are {} trees in this arroy index.", tree_stats.len());
+    let nb_roots = tree_stats.len();
+    println!("There are {} trees in this arroy index.", nb_roots);
     for (i, TreeStats { depth, dummy_normals }) in tree_stats.into_iter().enumerate() {
+        depth_sum += depth;
+        dummy_sum += dummy_normals;
         println!("Tree {i} as a depth of {depth} and {dummy_normals} dummy normals");
     }
+
+    println!(
+        "On average, got a depth of {} and {} normals",
+        depth_sum as f64 / nb_roots as f64,
+        dummy_sum as f64 / nb_roots as f64
+    );
 
     Ok(())
 }
