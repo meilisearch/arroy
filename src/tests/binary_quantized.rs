df654bca12cc0529212bdcfbf97a70b3bc46b280
Tamo <tamo@meilisearch.com>
2024-09-17 10:17:27 +0200

    update a test playing with -0.0 and 0.0 since -0.0 doesn't answer the same way to is_positive under x86 and aarch64

--- src/tests/binary_quantized.rs
+++ src/tests/binary_quantized.rs
@@ -14,7 +14,7 @@ fn write_and_retrieve_binary_quantized_vector() {
             &mut wtxn,
             0,
             &[
-                -2.0, -1.0, 0.0, -0.0, 2.0, 2.0, -12.4, 21.2, -2.0, -1.0, 0.0, 1.0, 2.0, 2.0,
+                -2.0, -1.0, 0.0, -0.1, 2.0, 2.0, -12.4, 21.2, -2.0, -1.0, 0.0, 1.0, 2.0, 2.0,
                 -12.4, 21.2,
             ],
         )
@@ -25,7 +25,7 @@ fn write_and_retrieve_binary_quantized_vector() {
         -1.0,
         -1.0,
         1.0,
-        1.0,
+        -1.0,
         1.0,
         1.0,
         -1.0,
@@ -47,7 +47,7 @@ fn write_and_retrieve_binary_quantized_vector() {
     insta::assert_snapshot!(handle, @r###"
     ==================
     Dumping index 0
-    Item 0: Leaf(Leaf { header: NodeHeaderBinaryQuantizedEuclidean { bias: 0.0 }, vector: [-1.0000, -1.0000, 1.0000, 1.0000, 1.0000, 1.0000, -1.0000, 1.0000, -1.0000, -1.0000, "other ..."] })
+    Item 0: Leaf(Leaf { header: NodeHeaderBinaryQuantizedEuclidean { bias: 0.0 }, vector: [-1.0000, -1.0000, 1.0000, -1.0000, 1.0000, 1.0000, -1.0000, 1.0000, -1.0000, -1.0000, "other ..."] })
     Tree 0: Descendants(Descendants { descendants: [0] })
     Root: Metadata { dimensions: 16, items: RoaringBitmap<[0]>, roots: [0], distance: "binary quantized euclidean" }
     "###);
